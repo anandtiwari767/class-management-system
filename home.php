53864b8f26e4e834838b6ded148ffb32b3c2b5c0
Anand tiwari <anandtiwari767@gmail.com>
2018-02-14 10:49:50 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<title>HOME</title>
+<link rel="stylesheet" href="home.css">
+</head>
+<body bgcolor="#eaeae1">
+<div id="menus"><!-- division of menus   -->
+<ul>
+  <li><a href="#">NEWS</a></li>
+  <li><a href="#">MANAGE ATTENDENCE</a></li>
+  <li><a href="#">QUERY</a></li>
+  <li><a href="#">VOTING</a></li>
+  <li><a href="#">CONTACT</a></li>
+  <li><a href="#">ABOUT US</a></li>
+  <li><a href="#">SIGN UP</a></li>
+	<li><a href="#">LOGIN</a></li>
+  </ul>
+</div>
+<div id="photo">
+<img src="student-cover-photo.jpg" width="1300px" height="500px"/>
+</div>
+<div>
+
+</div>
+<div id="footer">
+<h4 align="center">Maintained by ANAND TIWARI </h4>
+<h5 align="center">COPY RIGHTS ARE RESERVERD BY TECHACHE</h5>
+</div>
+</body>
+</html>
